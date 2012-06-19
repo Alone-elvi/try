6fdad420a7972d0d118b0e4f851fbd388490b0ed
alone-elvi <alone.elvi@gmail.com>
2012-06-20 03:32:17 +0400

    Глянь

--- index.php
+++ index.php
@@ -1,25 +1,47 @@
 <?php
-	class Main{
-		function __construct($r) {
-		    $this->req = explode('/', $r);
-		    $filename = '/'.strtolower($this->req[1]) . '.php';
-		    $file =  'controllers' . $filename;
-
-	    	if (file_exists($file) == false) {
-	            return false;
-		    }
-		    include ($file);
-		}
-
-
-	}
-	$main = new Main($_SERVER['REQUEST_URI']);
-	$cl = new $main->req[1];
-	
-	if(isset ($main->req[2])){
-		$cl->show();
+class Main{
+	function __construct(){
+		$this->req = $_SERVER['REQUEST_URI'];
+		trim($this->req,'/');
+		$this->req = explode("/", $this->req);		
 	}
 
-	error_reporting(E_ALL);
-    ini_set('display_errors', '1');
+	function setReq($add_req){
+		$this->req = $_SERVER['REQUEST_URI'];
+		trim($this->req,'/');
+		$this->req = explode("/", $this->req.$add_req);		
+	}
+}
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+$main = new Main;
+
+if(count($main->req)<=2){
+	$main->setReq('view');
+}
+else{
+	switch($main->req[2]){
+		case 'view' : break;
+		default 		: $main->setReq('view'); break;
+	}
+}
+
+if(isset($main->req[3])){
+	$req_arr=$main->req;
+	$req_arr[3]='show.php';
+	$request="";
+	$request.=implode("/", $req_arr);
+	$request[0]='/';
+	$file = $request;
+	if (! (file_exists($file)) ){ 
+		echo "Arbeiten".$file;
+		include($file);
+	}
+	else{
+		echo "Not Arbeiten";
+	}
+}
+
+
 ?>
