fecb7b3bfcd2abc52dcfaec9b1f7141976e496e8
Kane <kane.isturm@gmail.com>
2012-06-29 19:24:10 +0500

    some refactoring

---
+++ views/blog/find.php
@@ -0,0 +1,21 @@
+
+<html>
+
+<head>
+	<title></title>
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+
+</head>
+<body>
+	<div id="header">
+		<h1><?= $params['title'] ?></h1>
+	</div>
+	<div id="content">
+	</div>
+	<div>
+		<br>
+		<?php // echo $result['footer'];?>
+	</div>
+
+</body>
+</html>
