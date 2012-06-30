fdee8f2e2cc398112ef56dae727bc664d2d1ce43
alone-elvi <alone.elvi@gmail.com>
2012-06-30 17:33:06 +0400

    Revert "Revert "Теперь так""
    This reverts commit 86b2288e293e227184c6b8598ba74a3dc95f8d93.

--- controllers/blog.php
+++ controllers/blog.php
@@ -1,23 +1,17 @@
 <?php
-class Blog{
+class Blog extends Main{
 
-	function __construct($conf, $req){
-		$this->config = $conf;
-		$this->request = $req;
+	function __construct($parts){
+		$this->parts = $parts;
+		$this->dir = "{$this->parts['dirs']['views_dir']}/{$this->parts['controller']}";
 	}
-
+	
 	function render($params)
 	{
-		$file = "{$this->config['base_url']}/{$this->config['views']}/{$this->request[1]}/{$this->request[2]}.php";
-		if (! (file_exists($file))){
-			header("Status: 404 Not Found");
-			header('HTTP/1.0 404 Not Found');
-			echo 'File not found';
-			return false;
-		}
-		include($file);
+		$file = "{$this->dir}/{$this->parts['action']}.php";
+		$this->params = $params;
+		$this->include_file($file);
 	}
-
 	function show(){
 		$data = array(
 			'test' => 'Hello!'
