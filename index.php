d09cac27be8527ac91793113d21689270c6b6da6
alone-elvi <alone.elvi@gmail.com>
2012-06-30 17:19:38 +0400

    Теперь так

--- index.php
+++ index.php
@@ -1,38 +1,21 @@
 <?php
 
-require_once('config/conf.php');
+//require_once('config/conf.php');
 
 class Main{
 	function __construct(){
-		//require_once('config/conf.php');
-
-    $this->config = $GLOBALS['conf'];
-		$req = $_SERVER['REQUEST_URI'];
-		$this->request = explode("/", $req);
+		$this->request = $_SERVER['REQUEST_URI'];
+		$this->base_url = $_SERVER['DOCUMENT_ROOT'];
 	}
 
-	function LoadContr($request){
-		//$ret=false;
-		for ($i = 0; $i <count($this->config['controller']) ; $i++) {
-			$r = each($this->config['controller']);
-			if($r['key']==$request[1]){
-				$file = $this->config['base_url'].'/'.$this->config['controllers'].'/'.$this->config['controller'][$request[1]];
-				if (! (file_exists($file))){
-					$ret = false;
-				}
-				else{
-					include ($file);
-					$this->config['curr_req'] = $request[1];
-					$ret = true;
-					return $ret;
-				}
-			}
-			else{
-				$ret = false;
-			}
+	function include_file($file){
+		if (! (file_exists($file))){
+			header("Status: 404 Not Found");
+			header('HTTP/1.0 404 Not Found');
+			echo 'File not found';
+			return false;
 		}
-
-		return $ret;
+		include($file);
 	}
 }
 
@@ -40,21 +23,17 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 $main = new Main;
-$err = $main->LoadContr($main->request);
-if($err!=false){
-	$main->config['db_table']=$main->request[1];
+$main->include_file("controllers/router.php");
 
-	$contr_name = $main->request[1];
-	$contr = new $contr_name($main->config, $main->request);
+$router = new Router($main->request);
 
-	$action_name = $main->request[2];
+$contr_name = $router->arr_parts['controller'];
+$contr = new $contr_name($router->arr_parts);
+
+$action_name = $router->arr_parts['action'];
+if($action_name){
 	$contr->$action_name();
+}
 
-}
-else{
-	header("Status: 404 Not Found");
-	header('HTTP/1.0 404 Not Found');
-	echo 'File not found';
-	echo("<img src=\"/images/404.jpg\">");
-}
+
 ?>
